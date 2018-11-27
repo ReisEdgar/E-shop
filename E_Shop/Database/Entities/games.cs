d92f1a094de7c4d355f50304391048ab92ee76c3
Zygizz <tems107@gmail.com>
2018-11-27 19:58:16 +0200

    Prideta Katologu prekiu duombazes ( dar nenaudojamos niekur )

---
+++ E_Shop/Database/Entities/games.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Shop.Database.Entities
+{
+    public class games
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public string city { get; set; }
+        [ForeignKey("location")]
+        public virtual city location { get; set; }
+        public GameCategory Gamecategory { get; set; }
+        public string phonenumber { get; set; }
+
+    }
+
+
+    public enum GameCategory
+    {
+        ACTION,
+        Adventure,
+        Role_playing,
+        Simulation,
+        Strategy,
+        Sports
+
+
+    }
+}
+
