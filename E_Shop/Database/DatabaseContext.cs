a16817a90f6aaff8480cd039a0d06f1142fc2167
Marius Jurkenas <mjurkenas@gmail.com>
2018-12-05 20:13:24 +0200

    Started forum backend

--- E_Shop/Database/DatabaseContext.cs
+++ E_Shop/Database/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using E_Shop.Database.Entities;
+using E_Shop.Database;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,14 +12,18 @@ namespace E_Shop.Database
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-
+            ForumInitializer.ForumSeed(this);
         }
+
 
         public DbSet<Message> Messages { get; set; }
         public DbSet<Conversation> Conversations { get; set; }
         public DbSet<Hardware> HardwareRecords { get; set; }
         public DbSet<User> User { get; set; }
-         public DbSet<games> games { get; set; } /*
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<games> games { get; set; } /*
             public DbSet<Konsole> Konsole { get; set; }
             */
     }
---
+++ E_Shop/Logic/ForumService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using E_Shop.Database;
+using E_Shop.Database.Entities;
+using E_Shop.Dto;
+using E_Shop.Logic.Interfaces;
+
+namespace E_Shop.Logic
+{
+    public class ForumService : IForumService
+    {
+        private readonly DatabaseContext _context;
+        private readonly IMapper _mapper;
+
+        public ForumService(IMapper mapper, DatabaseContext context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void AddForum(ForumDto forum)
+        {
+            var toAdd = _mapper.Map<ForumDto, Forum>(forum);
+            /*if (toAdd.MessageType != MessageType.USER_TO_ADMIN_STANDART && toAdd.MessageType != MessageType.USER_TO_ADMIN_BROKEN_ITEM)
+            {
+                toAdd.ConversationId = _converstaionService.GetConversationIdByParticipants(message.SenderEmail, message.ReceiverEmail);
+            }*/
+            _context.Forums.Add(toAdd);
+            _context.SaveChanges();
+        }
+
+        public void DeleteForum(int id)
+        {
+            var result = _context.Forums.FirstOrDefault(x => x.Id == id);
+            _context.Forums.Remove(result);
+            _context.SaveChanges();
+        }
+
+        public ForumDto GetForumById(int id)
+        {
+            return _mapper.Map<Forum, ForumDto>(_context.Forums.FirstOrDefault(x => x.Id == id));
+        }
+
+        public void CreateForum()
+        {
+            
+        }
+    }
+}
